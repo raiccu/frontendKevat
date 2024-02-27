e1703ae50f378b1a72f954eac1bd603923e3ed04
Raikku <107673429+raiccu@users.noreply.github.com>
2024-02-27 13:25:36 +0200

    implemented ag grid

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,9 +1,25 @@
 import { useState } from "react";
-import TodoTable from "./TodoTable.jsx";
+import { AgGridReact } from "ag-grid-react";
+
+import "ag-grid-community/styles/ag-grid.css";
+import "ag-grid-community/styles/ag-theme-material.css";
 
 function TodoList() {
   const [todos, setTodos] = useState([]);
   const [descDate, setDescDate] = useState({ desc: "", date: "" });
+  const columnDefs = [
+    { field: "desc", headerName: "Description", filter: true,  floatingFilter: true },
+    { field: "date", headerName: "Date", filter: true, floatingFilter: true },
+    {
+      headerName: "Actions",
+      cellRenderer: deleteButtonRender,
+      cellRendererParams: {
+        onClick: (id) => {
+          deleteTodo(id);
+        },
+      },
+    },
+  ];
 
   const handleDescChange = (event) => {
     setDescDate({ ...descDate, desc: event.target.value });
@@ -27,22 +43,43 @@ function TodoList() {
   return (
     <div className="TodoList">
       <h2>Simple To-do List</h2>
-      <div className="form">
+      <div className="ag-theme-material">
         <input
+          className="ag-theme-material"
+          style={{ backgroundColor: "#fff", width: "33%" }}
           placeholder="Description"
           onChange={handleDescChange}
           value={descDate.desc}
         />
         <input
+          className="ag-theme-material"
+          style={{ backgroundColor: "#fff", width: "33%" }}
           placeholder="Date"
           onChange={handleDateChange}
           value={descDate.date}
         />
-        <button onClick={addTodo}>Add</button>
+        <button
+          className="ag-theme-material"
+          style={{ width: "34%", height: "2rem" }}
+          onClick={addTodo}
+        >
+          Add
+        </button>
+        <div className="ag-theme-material" style={{ width: 600, height: 500 }}>
+          <AgGridReact rowData={todos} columnDefs={columnDefs} />
+        </div>
       </div>
-      <TodoTable todos={todos} deleteTodo={deleteTodo}/>
     </div>
   );
 }
+
+const deleteButtonRender = (props) => {
+  const handleClick = () => {
+    props.onClick(props.data.id);
+  };
+
+  return <button className="ag-theme-material"
+  style={{width: "100%", height: "95%", border: "none" }} onClick={handleClick}>Delete</button>;
+};
 
 export default TodoList;
